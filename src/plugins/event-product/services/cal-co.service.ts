2cd8b9a4558143e18ad11db44b74ee7f94b7f5ad
Pavel Lobachev <pavellob@gmail.com>
2024-03-26 23:10:19 +0700

    feat: cal com hook

--- src/plugins/event-product/services/cal-co.service.ts
+++ src/plugins/event-product/services/cal-co.service.ts
@@ -4,13 +4,34 @@ https://docs.nestjs.com/providers#services
 
 import { HttpService } from "@nestjs/axios";
 import { Injectable } from "@nestjs/common";
+import { CustomerService, RequestContext } from "@vendure/core";
+import { Booking } from "../types";
 
 @Injectable()
 export class CalCoService {
-    constructor(private readonly httpService: HttpService) {}
+    constructor(
+      private readonly httpService: HttpService,
+      private customerService: CustomerService
+      ) {}
 
     findAll() {
+
       return this.httpService.axiosRef.get('/event-types').then(data => data.data.event_types.map((i: {link: string}) => console.log(i.link)));
+    }
+
+    handleHook(ctx: RequestContext, booking: Booking) {
+      switch (booking.triggerEvent) {
+        case "BOOKING_CREATED":
+          return this.customerService.create(ctx, {
+            emailAddress: booking.payload.attendees[0].email,
+            firstName: booking.payload.attendees[0].name.split(" ")[0],
+            lastName: booking.payload.attendees[0].name.split(" ")[1],
+          });
+          break;
+      
+        default:
+          break;
+      }
     }
       
 }
--- src/plugins/event-product/types.ts
+++ src/plugins/event-product/types.ts
@@ -7,11 +7,80 @@ import {
 declare module '@vendure/core/dist/entity/custom-entity-fields' {
     interface CustomProductVariantFields {
         isEvent: boolean;
+        externalBookingLink: string | null;
     }
     interface CustomShippingMethodFields {
         isEvent: boolean;
+        externalBookingLink: string | null;
     }
     interface CustomFulfillmentFields {
         eventUrls: string[] | null;
+    }
+}
+
+export interface Attendee {
+    email: string;
+    name: string;
+    firstName: string;
+    lastName: string;
+    timeZone: string;
+    language: { locale: string };
+    utcOffset: number;
+}
+
+export interface Organizer {
+    id: number;
+    name: string;
+    email: string;
+    username: string;
+    timeZone: string;
+    language: {
+        locale: string;
+    };
+    timeFormat: string;
+    utcOffset: number;
+}
+
+export interface Response {
+    label: string;
+    value?: string | string[];
+    isHidden: boolean;
+}
+
+export interface Booking {
+    triggerEvent: string;
+    payload: {
+        bookerUrl: string;
+        type: string;
+        title: string;
+        description: string;
+        additionalNotes: string;
+        customInputs: Record<string, any>;
+        startTime: string;
+        endTime: string;
+        organizer: Organizer;
+        responses: Record<string, Response>;
+        userFieldsResponses: Record<string, Response>;
+        attendees: Attendee[];
+        location: string;
+        destinationCalendar: { id: number; integration: string; externalId: string; primaryEmail: string; userId: number; eventTypeId: number | null; credentialId: number }[];
+        hideCalendarNotes: boolean;
+        requiresConfirmation: boolean;
+        eventTypeId: number;
+        seatsShowAttendees: boolean;
+        seatsPerTimeSlot: number;
+        seatsShowAvailabilityCount: boolean;
+        schedulingType: string;
+        iCalUID: string;
+        iCalSequence: number;
+        eventTitle: string;
+        eventDescription: string;
+        price: number;
+        currency: string;
+        length: number;
+        uid: string;
+        bookingId: number;
+        metadata: Record<string, any>;
+        status: string;
     }
 }
